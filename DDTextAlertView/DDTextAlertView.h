5b529d958adb6848b42235e853693929d4e2e40c
Danny <boy736809040@gmail.com>
2017-07-21 09:37:48 +0800

    Update DDTextAlertView.h

--- DDTextAlertView/DDTextAlertView.h
+++ DDTextAlertView/DDTextAlertView.h
@@ -5,6 +5,7 @@
 //  Created by SuperDanny on 16/4/27.
 //  Copyright © 2016年 SuperDanny ( http://SuperDanny.link/ ). All rights reserved.
 //
+//  当需要点击UIAlertView取消按钮时，不让UIAlertView立马消失，可以使用此控件
 
 #import <UIKit/UIKit.h>
 
